a9340d4cd6a1a03490f29ede211317bc69107012
Keaton Murray <murraykeaton422@gmail.com>
2025-07-03 22:45:29 -0400

    rebuild panel

--- panel/src/App.jsx
+++ panel/src/App.jsx
@@ -1,21 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Panel from './pages/Panel';
-import Sidebar from './components/Sidebar';
-import Topbar from './components/Topbar';
 
 function App() {
   return (
-    <div className="d-flex">
-      <Sidebar />
-      <div className="flex-grow-1">
-        <Topbar />
-        <main className="p-4">
-          <Routes>
-            <Route path="/" element={<Panel />} />
-          </Routes>
-        </main>
-      </div>
-    </div>
+      <Routes>
+        <Route path="/" element={<Panel />} />
+      </Routes>
   );
 }
 
--- panel/src/main.jsx
+++ panel/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './assets/css/index.css'
-import '@fontsource/poppins'
-import '@fontsource/poppins/300.css';
-import '@fontsource/poppins/400.css';
-import '@fontsource/poppins/600.css';
-import '@fontsource/poppins/700.css';
+import '@fontsource/inter';
+import '@fontsource/inter/400.css'; 
+import '@fontsource/inter/500.css'; 
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './assets/css/index.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
