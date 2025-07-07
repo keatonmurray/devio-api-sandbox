47b0b0a77235d5fa65600ee76f03fce99c2cd726
Keaton Murray <murraykeaton422@gmail.com>
2025-07-07 06:08:32 -0400

    Cleared web.php file

--- database/migrations/2025_07_07_094327_create_paypal_config_table.php
+++ database/migrations/2025_07_07_094327_create_paypal_config_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('mode')->default('sandbox');
             $table->timestamps();
         });
-
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,10 @@
 <?php
 
-use App\Http\Controllers\plugins\PayPalController;
-use Illuminate\Support\Facades\Route;
+/**
+ * Nothing interesting here, just a placeholder to avoid confusion.
+ * 
+ * This file is not used in the current implementation. 
+ * 
+ * All POST requests are handled by the GraphQL endpoint at /graphql.
+ * All GET requests are handled by React Router.
+ */
