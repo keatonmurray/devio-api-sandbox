14d5667562eac4f459677efd44c6118a7535aaa2
Keaton Murray <keaton.murray@coalitiontechnologies.com>
2025-09-20 23:51:24 -0700

    merge branch

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -10,14 +10,16 @@ import { Copy } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className="main-homepage d-flex align-items-center justify-content-center flex-column">
+    <div className="devio-main-homepage d-flex align-items-center justify-content-center flex-column">
+      
+      
       <div className="w-75">
          {/* Homepage Content */}
         <h1 className="display-2 fw-bolder text-center section-heading-title">
           Build <span className="text-highlight">Modern Content</span> APIs in Minutes
         </h1>
         <p className="text-secondary mt-3 w-md-75 w-100 text-center mx-auto">
-          A headless CMS that's fast, flexible, and built for developers. Clean and intuitive admin panel, designed to manage content effortlessly for anything from simple websites to complex applications.
+          A headless CMS that's fast, flexible, and built for developers. Clean and intuitive admin panel, designed to manage content effortlessly.
         </p>
 
         {/* Code Editor */}
